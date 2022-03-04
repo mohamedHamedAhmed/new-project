f66777f8b78d0151b8b9777d0027dc56eb20ad09
mohamed hamed <mohamed@gmail.com>
2022-03-04 15:27:02 +0200

    last commit

---
+++ test.js
@@ -0,0 +1,10 @@
+
+let menuItems = document.getElementById('menuItems');
+menuItems.style.maxHeight = "0";
+function menuToggle() {
+    if (menuItems.style.maxHeight == "0px") {
+        menuItems.style.maxHeight = "200px";
+    } else {
+        menuItems.style.maxHeight = "0px";
+    }
+}
